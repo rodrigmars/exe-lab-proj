3293b0e17e5328f700e7c97d3e6f0b0ca72ec190
rodrigmars <rodrigmars@gmail.com>
2023-10-30 18:43:58 -0300

    Lógica para DisplayCreditsMenu refeita

--- credit_score_sim.c
+++ credit_score_sim.c
@@ -1,8 +1,24 @@
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
+
 #include <stdio.h>   //<-- Para função printf
 #include <string.h>  //<-- Para uso com string e função strlen()
 #include <ctype.h>   //<-- Para função toupper()
 #include <Windows.h> //<-- Para função system()
+
 // #include <conio.h> // pensando aqui se devo realmente usá-lo **
+
+typedef struct
+{
+    float credit;
+    int status;
+} Credits;
 
 // function prototype
 char GetCharInput(char *chr, int size);
@@ -13,7 +29,7 @@ void ExitProgram(char *input, int *attempts);
 
 void message();
 
-void DisplayCreditsMenu(float *coin);
+void DisplayCreditsMenu(float *coin, Credits credits[][2]);
 
 int main()
 {
@@ -91,57 +107,58 @@ void ExitProgram(char *input, int *attempts)
 
 void RunningCreditCheck(char *input, float *coin)
 {
-    float credits = 0.0;
+    Credits credits[3][2] = {{15.5, 1}, {25.5, 1}, {50.5, 1}};
 
     int run = 1;
+    // 1:true
+    // 0:false
 
     while (run)
     {
 
         system("cls");
-        DisplayCreditsMenu(coin);
+        DisplayCreditsMenu(coin, credits);
         GetCharInput(input, 2);
         system("pause");
 
         switch (input[0])
         {
         case '1':
-            credits = 15.5;
-            if (*coin < credits)
+            if (*coin < credits[0]->credit)
             {
                 message();
+                credits[0]->status = 0;
                 continue;
             }
 
-            *coin -= credits;
+            *coin -= credits[0]->credit;
 
             break;
 
         case '2':
 
-            credits = 25.5;
-
-            if (*coin < credits)
+            if (*coin < credits[1]->credit)
             {
                 message();
+                credits[1]->status = 0;
                 continue;
             }
 
-            *coin -= credits;
+            *coin -= credits[1]->credit;
 
             break;
 
         case '3':
 
-            credits = 50.5;
-
-            if (*coin < credits)
+            if (*coin < credits[2]->credit)
             {
                 message();
+                credits[2]->status = 0;
                 continue;
             }
 
-            *coin -= credits;
+            *coin -= credits[2]->credit;
+
             break;
 
         case '4':
@@ -162,12 +179,20 @@ void message()
     system("pause");
 }
 
-void DisplayCreditsMenu(float *coin)
+void DisplayCreditsMenu(float *coin, Credits credits[][2])
 {
-    printf("\n\nTotal em créditos: %.2f\n\n", *coin);
-    printf("Selecione uma operação [x]\n\n");
-    printf("[1] > Crédito: 15.5\n");
-    printf("[2] > Crédito: 25.5\n");
-    printf("[3] > Crédito: 50.5\n");
-    printf("[4] > Sair\n\n_");
+    printf(KYEL "\n\nTotal em créditos: " KGRN "%.2f\n\n", *coin);
+    printf(KYEL "Selecione uma operação [x]\n\n");
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        if (credits[i]->status == 1)
+            printf(KGRN "[%i] > Crédito: ", i + 1);
+        else
+            printf(KRED "[%i] > Crédito: ", i + 1);
+
+        printf("%.2f\n", credits[i]->credit);
+    }
+
+    printf(KYEL "[4] > Sair\n\n_" KWHT);
 }
