c36169989e741bcb1c8b3a9116b2e0d6c8b7a475
rodrigmars <rodrigmars@gmail.com>
2023-10-29 21:09:44 -0300

    chcp implementado para suportar acentos

--- exercise_01.c
+++ exercise_01.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <Windows.h>
+
+char GetInput(char *chr, int size)
+{
+    int i = 0;
+
+    while ((chr[i] = getchar()) != '\n' && i < size)
+        i++;
+
+    chr[++i] = '\0';
+}
 
 int main()
 {
-    char message[] = "Hello, world";
 
-    printf("%s\n", message);
+    system("chcp 65001");
+
+    char input[2];
+
+    printf("Você concorda?_");
+
+    GetInput(input, 2);
+
+    if (input[0] == 'y' || input[0] == 'Y')
+    {
+        printf("Acordado.\n");
+    }
+    else if (input[0] == 'n' || input[0] == 'N')
+    {
+        printf("Não acordado.\n");
+    }
+    else
+    {
+        printf("Opção não compatível.\n");
+    }
 
     return 0;
 }
